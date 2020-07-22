07b25e7f8e4cf628d2fb73d8136a9395ba788396
Steven Stieglitz <stevens714@yahoo.com>
2020-07-22 18:53:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ function happyHolidays() {
   return "Happy holidays!";
 }
 
-function happyHolidaysTo(name) {return `Happy holidays, ${name}!`
+function happyHolidaysTo(name) {
+return `Happy holidays, ${name}!`
